da4b2de68dc4e42b164d2a24c44c9f4b9f63ebf0
Luiz Chaves <lucachaves@gmail.com>
2025-09-26 17:50:36 -0300

    exercise: function-calc

---
+++ exercises/function-calc/calc.js
@@ -0,0 +1,17 @@
+/**
+ * operator: '+', '-', '*', '/'
+ */
+export function calc(operand1, operand2, operator) {
+  switch (operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '*':
+      return operand1 * operand2;
+    case '/':
+      return operand1 / operand2;
+    default:
+      throw new Error('Invalid operator');
+  }
+}
