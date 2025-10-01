788084646b1408044332c1c8f9ebbeb5ab6f317e
Luiz Chaves <lucachaves@gmail.com>
2025-10-01 17:33:39 -0300

    classroom: Refactor array operation functions to improve readability and maintainability

--- exercises/array-operations/array-operations.js
+++ exercises/array-operations/array-operations.js
@@ -1,43 +1,51 @@
 export function sum(array) {
-  if (!array.length) {
-    return NaN;
-  }
+  // if (!array.length) {
+  //   return NaN;
+  // }
 
-  let total = 0;
+  // let total = 0;
 
-  for (const value of array) {
-    total += value;
-  }
+  // for (const value of array) {
+  //   total += value;
+  // }
 
-  return total;
+  // return total;
+  // return array.length && array.reduce((acc, curr) => acc + curr, 0);
+  return array.length ? array.reduce((acc, curr) => acc + curr, 0) : NaN;
 }
 
 export function product(array) {
-  if (!array.length) {
-    return NaN;
-  }
+  // if (!array.length) {
+  //   return NaN;
+  // }
 
-  let total = 1;
+  // let total = 1;
 
-  for (const value of array) {
-    total *= value;
-  }
+  // for (const value of array) {
+  //   total *= value;
+  // }
 
-  return total;
+  // return total;
+  return array.length ? array.reduce((acc, curr) => acc * curr) : NaN;
 }
 
 export function sumOdds(array) {
-  if (!array.length) {
-    return NaN;
-  }
+  // if (!array.length) {
+  //   return NaN;
+  // }
 
-  let total = 0;
+  // let total = 0;
 
-  for (const value of array) {
-    if (value & 1) {
-      total += value;
-    }
-  }
+  // for (const value of array) {
+  //   if (value & 1) {
+  //     total += value;
+  //   }
+  // }
 
-  return total;
+  // return total;
+  return array.length
+    ? array
+      .filer((n) => n & 1)
+      .reduce((acc, curr) => acc + curr, 0)
+    : NaN;
 }
