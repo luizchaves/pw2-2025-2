b85152d08788f833fa590a58149ee1ae67ba9964
Luiz Chaves <lucachaves@gmail.com>
2025-11-28 16:47:34 -0300

    refactor: Add About page and remove Hello page

---
+++ classroom/packages/nextjs/invest-app/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h1 className="p-5 text-center text-3xl">About Invest App</h1>;
+}
--- classroom/packages/nextjs/invest-app/app/hello/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Hello() {
-  return <h1 className="p-5 text-center text-3xl">Hello, Invest App!</h1>;
-}
